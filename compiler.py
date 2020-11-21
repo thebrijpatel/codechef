c77574131360a89f4a1dd665cc7b371c9f068128
thebrijpatel <sadariyabrijesh@gmail.com>
2020-11-21 21:33:16 +0530

    compiler success

--- compiler.py
+++ compiler.py
@@ -1,14 +1,21 @@
-N=int(input())
-for _ in range(N):
-    e=input()
-    a=0
-    ans=0
-    for i in e:
-        if i == '<':
-            a+=1
-        else:
-            a-=1
-            ans=ans+1
-            if ans<0:
-                break
-    print(ans)
+try:
+    N=int(input())
+    for _ in range(N):
+        s=input()
+        one=0
+        two=0
+        ans=0
+        for i in s:
+            if i=='<':
+                one+=1
+            else:
+                one-=1
+                two+=1
+                if one==0:
+                    ans+=two*2
+                    two=0
+                elif one<0:
+                    break
+        print(ans)
+except:
+    pass
